5c6407c3aab3570f23dd4372be8514a04300002c
Sandeep Madnaik <smadnaik@e1z2r3p13.42.us.org>
2018-06-06 00:43:27 -0700

    Final code for GetLextLine.

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -1,37 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smadnaik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/30 01:42:20 by smadnaik          #+#    #+#             */
+/*   Updated: 2018/05/30 02:36:58 by smadnaik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
+
+/*
+** This method handles a line at a time. Reads into buffer.
+** If end of string is reached, it calls get_next_line() to read again.
+*/
 
 int		ft_new_line(char **s, char **line, int fd, int ret)
 {
-	char	*tmp;
+	char	*temp;
 	int		len;
 
 	len = 0;
-	while (s[fd][len] != '\n' && s[fd][len] != '\0')
+	while (s[fd][len] != '\0' && s[fd][len] != '\n')
 		len++;
-	if (s[fd][len] == '\n')
-	{
-		*line = ft_strsub(s[fd], 0, len);
-		tmp = ft_strdup(s[fd] + len + 1);
-		free(s[fd]);
-		s[fd] = tmp;
-		if (s[fd][0] == '\0')
-			ft_strdel(&s[fd]);
-	}
-	else if (s[fd][len] == '\0')
+	if (s[fd][len] == '\0')
 	{
 		if (ret == BUFF_SIZE)
 			return (get_next_line(fd, line));
 		*line = ft_strdup(s[fd]);
 		ft_strdel(&s[fd]);
 	}
+	else if (s[fd][len] == '\n')
+	{
+		*line = ft_strsub(s[fd], 0, len);
+		temp = ft_strdup(s[fd] + len + 1);
+		free(s[fd]);
+		s[fd] = temp;
+		if (s[fd][0] == '\0')
+			ft_strdel(&s[fd]);
+	}
 	return (1);
 }
+
+/*
+**	This method reads into buffer and keeps sync among multiple fds by using
+**	strjoin. It calls ft_new_line if it has more lines to read.
+*/
 
 int		get_next_line(const int fd, char **line)
 {
 	static char	*s[255];
 	char		buf[BUFF_SIZE + 1];
-	char		*tmp;
+	char		*temp;
 	int			ret;
 
 	if (fd < 0 || line == NULL)
@@ -41,9 +63,9 @@ int		get_next_line(const int fd, char **line)
 		buf[ret] = '\0';
 		if (s[fd] == NULL)
 			s[fd] = ft_strnew(1);
-		tmp = ft_strjoin(s[fd], buf);
+		temp = ft_strjoin(s[fd], buf);
 		free(s[fd]);
-		s[fd] = tmp;
+		s[fd] = temp;
 		if (ft_strchr(buf, '\n'))
 			break ;
 	}
--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smadnaik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/30 01:42:28 by smadnaik          #+#    #+#             */
+/*   Updated: 2018/05/30 04:41:42 by smadnaik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 16
 
 # include "fcntl.h"
 # include "sys/types.h"
 # include "sys/uio.h"
 # include "unistd.h"
 # include "stdlib.h"
-# include "libft.h"
+# include "libft/libft.h"
 
 int		ft_new_line(char **s, char **line, int fd, int ret);
 int		get_next_line(const int fd, char **line);
